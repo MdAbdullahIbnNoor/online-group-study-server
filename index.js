288a1a09aae3bd51564d6dd218c79fafad2e37bc
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-11-08 03:31:08 +0600

    some update done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
         await client.connect();
 
         const assignmentCollection = client.db('groupStudy').collection('assignments');
+        const myAssignmentCollection = client.db('groupStudy').collection('myAssignments');
 
         app.get('/assignment', async (req, res) => {
             const cursor = assignmentCollection.find();
@@ -66,12 +67,89 @@ async function run() {
             try {
                 const result = await assignmentCollection.updateOne(filter, assignmentUpdates);
                 res.send(result);
-                
+
             } catch (error) {
                 console.error('Error updating assignment:', error);
                 res.status(500).json({ error: 'Internal server error' });
             }
         })
+
+        app.post('/myAssignment', async (req, res) => {
+            const assignment = req.body;
+            console.log(assignment);
+            const result = await myAssignmentCollection.insertOne(assignment);
+            res.send(result);
+        });
+
+        app.get('/myAssignment', async (req, res) => {
+            const cursor = myAssignmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/myAssignment/filter', async (req, res) => {
+            const { status } = req.query; // Get the 'status' query parameter
+
+            const query = { status: status }
+            const result = await myAssignmentCollection.find(query).toArray();
+            // console.log(result, status);
+
+            res.json(result);
+        });
+
+        app.put('/myAssignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedAssignment = req.body;
+
+            const assignmentUpdates = {
+                $set: {
+                    title: updatedAssignment.title,
+                    description: updatedAssignment.description,
+                    marks: updatedAssignment.marks,
+                    difficultyLevel: updatedAssignment.difficultyLevel,
+                    dueDate: updatedAssignment.dueDate,
+                    photoURL: updatedAssignment.photoURL,
+                    status: updatedAssignment.status,
+                    pdfLink: updatedAssignment.pdfLink,
+                    additionalText: updatedAssignment.additionalText,
+                    submittedBy: updatedAssignment.submittedBy
+                }
+            };
+
+            try {
+                const result = await assignmentCollection.updateOne(filter, assignmentUpdates);
+                res.send(result);
+
+            } catch (error) {
+                console.error('Error updating assignment:', error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        })
+
+        app.patch('/myAssignment/markUpdate/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedAssignment = req.body;
+            console.log(updatedAssignment);
+            const updateDoc = {
+                $set: {
+                    status: updatedAssignment.status,
+                    markGiven: updatedAssignment.markGiven,
+                    feedBack: updatedAssignment.feedBack,
+                },
+            };
+            const result = await myAssignmentCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/myAssignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await myAssignmentCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
